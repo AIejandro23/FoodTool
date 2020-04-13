b376076896c25df28e8c76e54c96a9d6f51962ac
AIEJANDRO23 <wateralexpolo@gmail.com>
2020-04-13 16:12:49 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+var express = require('express');
+var app = express();
+var food = require('./food');
+var path = require('path');
+const ejs = require("ejs");
+
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set("view engine", "html");
+
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.get('/food/', food.get);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
---
+++ food.js
@@ -0,0 +1,28 @@
+var request = require('request');
+
+exports.get = function(req, res, body){
+     request({
+
+        uri: 'https://world.openfoodfacts.org/api/v0/product/' + req.query.codeBar
+
+      },function(error,response,body){
+
+        var bodyJSON = JSON.parse(body);
+
+        if(bodyJSON.product != null){
+            var productImage = bodyJSON.product.image_url;
+            var productName = bodyJSON.product.product_name;
+            var productKCal100 = bodyJSON.product.nutriments.energy_value;
+            var productFat100 = bodyJSON.product.nutriments.fat;
+            var productCH100 = bodyJSON.product.nutriments.carbohydrates_100g;
+            var productProtein100 = bodyJSON.product.nutriments.proteins_value;
+            var productSalt100 = bodyJSON.product.nutriments.salt_value;
+
+            res.render('food', {'productName':productName, 'productImage':productImage, 'productKCal100':productKCal100,'productFat100': productFat100,
+                                'productCH100':productCH100, 'productProtein100':productProtein100, 'productSalt100':productSalt100});
+
+        }else{
+            res.send("404 Product not found");
+        }
+     });
+};
